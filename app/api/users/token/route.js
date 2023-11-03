5220eb53088e1839ac4d8afc81625d0e83d9a62e
xeviabcd28 <xeviabcd28@gmail.com>
2023-11-03 14:52:04 +0600

    bug fix

--- app/api/users/token/route.js
+++ app/api/users/token/route.js
@@ -3,12 +3,14 @@ import jwt_decode from "jwt-decode";
 
 export async function GET(request) {
     const { cookies } = request;
-    const jwt = cookies._parsed.get("token").value;
-    // console.log(cookies._parsed.get("token").value)
-    if (!jwt) {
-        return NextResponse.json({ message: "Invalid Token" })
-    }
-    const decodedToken = jwt_decode(jwt);
-    return NextResponse.json({ decodedToken })
+    const tokenCookie = cookies?._parsed?.get("token");
 
+    if (!tokenCookie) {
+        return NextResponse.json({ message: "Invalid Token" });
+    }
+
+    const jwt = tokenCookie.value;
+    const decodedToken = jwt_decode(jwt);
+
+    return NextResponse.json({ decodedToken });
 }
